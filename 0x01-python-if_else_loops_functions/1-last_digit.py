078d63e0e480088914fef523f1859c36dc6ddede
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-12-14 05:42:31 -0800

    modulos %

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
 else:
     rem = int (number_string[-1])
 if rem > 5:
-    print("The last digit of {} is {} and is greater than 5".format(number,rem))
+    print("Last digit of {} is {} and is greater than 5".format(number,rem))
 elif rem == 0:
-    print("The last digit of {} is {} and is zero".format(number,rem))
+    print("Last digit of {} is {} and is zero".format(number,rem))
 elif rem < 6 and not 0:
-    print("The last digit of {} is {} and is less than 6 and not 0".format(number,rem))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number,rem))
