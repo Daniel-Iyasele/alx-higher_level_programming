ca0fc749cc0297b3e437e3a06c8de6d6c303caf3
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-01-11 08:11:41 -0800

    print 3X3 matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) != 0:
+        column_length = len(matrix[0])
+        row_length = len(matrix)
+        for row in range(row_length):
+            for column in range(column_length):
+                print("{:d}".format(matrix[row][column]), end="")
+                if column < 2:
+                    print(" ",end="")
+                else:
+                    print()
