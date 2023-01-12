1cf45b122c76d897ac36b3be4fd6e79a4f49223d
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-01-12 02:59:08 -0800

    max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+    elif len(my_list) == 1:
+        return my_list[0]
     else:
         length = len(my_list) - 1
         for i in range(length):
