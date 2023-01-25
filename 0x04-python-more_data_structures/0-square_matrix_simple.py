884107046de08a0f6bbc2e48f632692db2a56f3a
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-01-25 08:08:57 -0800

    square of list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
+    """new_matrix = matrix
     return [[new_matrix[i][j] ** 2 for j in range(len(matrix))]
-            for i in range(len(matrix[0]))]
+            for i in range(len(matrix[0]))]"""
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
