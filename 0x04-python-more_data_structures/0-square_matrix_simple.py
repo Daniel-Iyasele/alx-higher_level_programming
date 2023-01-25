0955017ba474cd49af6d1f8760fafcfa8e8533fe
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-01-25 08:01:36 -0800

    more data struct

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix =matrix
-    return[[new_matrix[j][i]**2 for i in range(len(matrix))]\
-            for j in range(len(matrix[0]))]
+    return[[new_matrix[i][j]**2 for j in range(len(matrix))]\
+            for i in range(len(matrix[0]))]
