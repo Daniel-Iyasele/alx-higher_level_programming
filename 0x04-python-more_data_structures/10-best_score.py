96f0d4f0af5d1dba206f4e795067a784fa373932
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-02-04 15:34:24 -0800

    best key score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best = ""
+    k = list(a_dictionary)
+    for i in range(len(a_dictionary)):
+        value = a_dictionary[k[i]]
+        key = k[i]
+        if (i + 1) < len(a_dictionary) and a_dictionary[k[i + 1]] > value:
+            best = k[i + 1]
+    return best
