7d2ecf3e69c3e7125196639742b8e3544d24c27e
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-02-04 13:40:03 -0800

    roman numeral

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """This funcion converts roman numerals to integers
+     variables:
+      Result: stores the outcome of the roman numerals
+      Num: A dicionary that uses roman numerals as keys and\
+              their corresponding integers as values
+    """
+    if roman_string is None or isinstance(roman_string, str) is False:
+        return None
+    roman_string = roman_string.upper()
+    result = 0
+    num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i in range(len(roman_string)):
+        value = num[roman_string[i]]
+        if (i + 1) < len(roman_string) and num[roman_string[i + 1]] > value:
+            result -= value
+        else:
+            result += value
+    return result
