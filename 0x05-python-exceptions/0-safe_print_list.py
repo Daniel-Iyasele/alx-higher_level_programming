3f6a7bae6e9b19b7c9cad91ba0c06cc7478d5f61
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-03-04 15:29:40 -0800

    first commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    new = []
+    count = 0;
     try:
         for i, v in enumerate(my_list):
             if i == x:
                 break
             else:
                 print(v, end="")
+                count += 1
         print()
-        return v
+        return count
     except UnknownError:
         print("Unknown error occured")
