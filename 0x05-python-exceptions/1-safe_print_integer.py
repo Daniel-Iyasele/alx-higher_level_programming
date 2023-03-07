93f36380782e2ef37d90eede3c9231b93cb7531d
Daniel-Iyasele <charlesiregbe007@gmail.com>
2023-03-07 07:31:05 -0800

    print ineger

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if value:
-            print("{:d}".format(value))
+            print("{:d}".format(value), end='\n')
             return True
         else:
             return False
